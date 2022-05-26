e46014253dcf447b048cb7b6d5d0449b4301bc2b
NguyenDo13 <84115673+NguyenDo13@users.noreply.github.com>
2022-05-26 15:49:32 +0700

    put module folders in public folder

---
+++ Porto-web/public/module-23/23.php
@@ -0,0 +1,7 @@
+<div class="title">
+     <h4 class="name-cart">Shopping Cart</h4>
+     <i class="fa fa-angle-right"></i>
+     <h4>Checkout</h4>
+     <i class="fa fa-angle-right"></i>
+     <h4>Order Complete</h4>
+</div>
--- Porto-web/routes/web.php
+++ Porto-web/routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+
+Route::get('/cart', function () {
+    return view('cart');
+});
+
