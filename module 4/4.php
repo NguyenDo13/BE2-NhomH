5204ce7bc239edb84e1ffa08102174f4d4efd239
voyen057 <voyen057@gmail.com>
2022-05-27 23:45:14 +0700

    edit module 4

---
+++ module 4/4.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>4</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+</head>
+
+<body>
+    <div class="type-4">
+        <div id="carouselExampleControlsNoTouching" class="carousel slide" data-bs-touch="false"
+            data-bs-interval="false">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="images/shop4_home_slider1.jpg" class="d-block w-100" alt="...">
+                    <div class="card-img-overlay " style="padding: 50px 0 0 100px;">
+                        <h3 class="card-title " style="color: gray;font-size: 18px;"> Find the Boundaries. Push Through!
+                        </h3>
+                        <h2 style="font-size: 48px;line-height: 1;font-weight:700;letter-spacing:.005em"
+                            class="vc_custom_heading mb-0 align-left">Summer Sale</h2>
+                        <h3 style="font-size: 76px;line-height: 1" class="vc_custom_heading mb-3 align-left">70% OFF
+                        </h3>
+                        <div class="vc_row wpb_row vc_inner row">
+                            <div class="vc_column_container col-md-12">
+                                <div class="wpb_wrapper vc_column-inner" style="display: flex;">
+                                    <h4 style="font-size: 1.2em;line-height: 1"
+                                        class="vc_custom_heading me-3 pull-left p-t-sm ls-n-20 align-left text-uppercase">
+                                        Starting At</h4>
+                                    <h4 style="font-size: 1.8em;line-height: 1"
+                                        class="vc_custom_heading coupon-sale-text d-inline-block me-2 align-left">
+                                        <b><sup>$</sup>199<sup>99</sup></b>
+                                    </h4>
+                                    <div class="vc_btn3-container mb-0 vc_btn3-inline"><button
+                                            class="vc_btn3 vc_btn3-shape-default wpb_custom_4eba9bcc8f6c36e45e1d476fc3d9024c btn btn-modern btn-lg btn-dark">Shop
+                                            Now!</button></div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="carousel-item ">
+                    <img src="images/shop4_home_slider2.jpg" class="d-block w-100" alt="...">
+                    <div class="card-img-overlay" style="padding: 50px 100px 0 800px;">
+                        <div class="wpb_wrapper vc_column-inner">
+                            <h4 style="font-size: 28px;color: #999999;line-height: 1;font-weight:500"
+                                class="vc_custom_heading mb-1 align-left">EXTRA</h4>
+                            <h3 style="font-size: 66px;line-height: 1" class="vc_custom_heading mb-2 align-left">20%
+                                OFF</h3>
+                            <div
+                                class="vc_custom_heading mb-3 align-left heading heading-border heading-dark heading-middle-border heading-middle-border-center heading-border-xl">
+                                <h4 style="font-size: 38px;line-height: 1;font-weight: bold;" class="heading-tag">
+                                    -ACCESSORIES-</h4>
+                            </div>
+                            <h2 style="font-size: 44px;line-height: 1;font-weight:500;letter-spacing:.005em"
+                                class="vc_custom_heading mb-4 align-left">Summer Sale</h2>
+                            <div class="vc_btn3-container ls-150 mb-0 vc_btn3-center"><button
+                                    class="vc_btn3 vc_btn3-shape-default vc_btn3-block wpb_custom_4eba9bcc8f6c36e45e1d476fc3d9024c btn btn-modern btn-xl btn-dark">SHOP
+                                    ALL SALE</button></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching"
+                data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControlsNoTouching"
+                data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+</body>
+
+</html>
