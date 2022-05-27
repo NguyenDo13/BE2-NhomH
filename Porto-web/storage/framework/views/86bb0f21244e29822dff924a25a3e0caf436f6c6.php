63dd089d104c667edb9dacf6cabef5f368538a0c
voyen057 <voyen057@gmail.com>
2022-05-28 05:39:48 +0700

    edit module 15

---
+++ Porto-web/storage/framework/views/86bb0f21244e29822dff924a25a3e0caf436f6c6.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <title>Home</title>
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+          integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+          crossorigin="anonymous" referrerpolicy="no-referrer" />
+          <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+     <link href="<?php echo e(asset('css/bootstrap.min.css')); ?>" rel="stylesheet" type="text/css" />
+     <link rel="stylesheet" href="<?php echo e(asset('css/style.css')); ?>">
+</head>
+<body>
+<?php include public_path('module-22/22.php')?>
+<?php include public_path('module-4/4.php')?>
+ <?php include public_path('module-5/5.php')?>
+ <?php include public_path('module-6/6.php')?>
+</body>
+</html><?php /**PATH D:\BE2-NhomH\Porto-web\resources\views/home.blade.php ENDPATH**/ ?>
