426efae71bf6c9703d606325fbfdf335fa489106
NguyenDo13 <84115673+NguyenDo13@users.noreply.github.com>
2022-05-29 14:04:18 +0700

    edit carts, detail_product, home files

--- Porto-web/resources/views/clients/pages/carts.blade.php
+++ Porto-web/resources/views/clients/pages/carts.blade.php
@@ -1,27 +1,3 @@
-{{-- <!DOCTYPE html>
-<html lang="en">
-<head>
-     <meta charset="UTF-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-     <title>Document</title>
-     
-     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
-     integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
-     crossorigin="anonymous" referrerpolicy="no-referrer" />
-     <link rel="stylesheet"
-     href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-     <link href="{{ asset('assets/clients/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
-     <link rel="stylesheet" href="{{ asset('assets/clients/css/style.css') }}">
-</head>
-<body>
-     
-
-     @include('clients.modules.module-23')
-     @include('clients.modules.module-25')
-
-</body>
-</html> --}}
 
 @extends('layouts.master')
 
