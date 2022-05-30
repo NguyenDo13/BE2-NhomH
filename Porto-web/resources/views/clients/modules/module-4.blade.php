24ae5f9f5299dff0b186704f502ec9d00c077e4e
voyen057 <voyen057@gmail.com>
2022-05-31 01:02:11 +0700

    edit module 4

--- Porto-web/resources/views/clients/modules/module-4.blade.php
+++ Porto-web/resources/views/clients/modules/module-4.blade.php
@@ -4,7 +4,7 @@
             data-bs-interval="false">
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img src="{{ asset('assets/clients/images/shop4_home_slider1.png') }}" class="d-block w-100" alt="...">
+                    <img src="{{ asset('assets/clients/images/shop4_home_slider1.png') }}" class="d-block w-100" alt="">
                     <div class="card-img-overlay " style="padding: 50px 0 0 100px;">
                         <h3 class="card-title " style="color: gray;font-size: 18px;"> Find the Boundaries. Push Through!
                         </h3>
@@ -15,15 +15,15 @@
                         <div class="vc_row wpb_row vc_inner row">
                             <div class="vc_column_container col-md-12">
                                 <div class="wpb_wrapper vc_column-inner" style="display: flex;">
-                                    <h4 style="font-size: 1.2em;line-height: 1"
+                                    <h4 style="font-size: 1.2em;line-height: 1; font-weight: bold;padding: 10px"
                                         class="vc_custom_heading me-3 pull-left p-t-sm ls-n-20 align-left text-uppercase">
                                         Starting At</h4>
-                                    <h4 style="font-size: 1.8em;line-height: 1"
+                                    <h4 style="font-size: 1.8em;line-height: 1;border: 1px solid #ff7373; background: #ff7373;color: white;padding: 10px 5px;transform: rotate(-2deg) "
                                         class="vc_custom_heading coupon-sale-text d-inline-block me-2 align-left">
-                                        <b><sup>$</sup>199<sup>99</sup></b>
+                                        <b style="font-size: 25px"><sup>$</sup>199<sup>99</sup></b>
                                     </h4>
-                                    <div class="vc_btn3-container mb-0 vc_btn3-inline"><button
-                                            class="vc_btn3 vc_btn3-shape-default wpb_custom_4eba9bcc8f6c36e45e1d476fc3d9024c btn btn-modern btn-lg btn-dark">Shop
+                                    <div class="vc_btn3-container mb-0 vc_btn3-inline" ><button
+                                            class="vc_btn3  wpb_custom_4eba9bcc8f6c36e45e1d476fc3d9024c btn btn-modern btn-lg btn-dark" style="border-radius: 0px; padding:10px 30px; font-weight: bold;font-size: 20px">Shop
                                             Now!</button></div>
                                 </div>
                             </div>
