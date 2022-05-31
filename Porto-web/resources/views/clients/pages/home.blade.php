8f5cfd036ae0a6f093e7c77a714b2e812fd2e9f8
Taiemcute <85283737+Taiem15032002@users.noreply.github.com>
2022-05-31 15:05:30 +0700

    module 9

--- Porto-web/resources/views/clients/pages/home.blade.php
+++ Porto-web/resources/views/clients/pages/home.blade.php
@@ -23,5 +23,6 @@
 @include('clients.modules.module-4')
 @include('clients.modules.module-5')
 @include('clients.modules.module-6')
+@include('clients.modules.module-9')
 @include('clients.modules.module-10')
 @endsection
