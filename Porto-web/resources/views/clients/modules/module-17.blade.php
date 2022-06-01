aac6af0a2b6b0c7c2f803b86773d4e6ff378dbc1
voyen057 <voyen057@gmail.com>
2022-06-01 08:22:55 +0700

    include module 17

--- Porto-web/resources/views/clients/modules/module-17.blade.php
+++ Porto-web/resources/views/clients/modules/module-17.blade.php
@@ -1,25 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
+<div class="module-17">
+    <div class="container">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>17</title>
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css" type="text/css">
-    <!-- Font awesome -->
-    <link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.min.css" type="text/css">
-
-    <!-- Custom css - Các file css do chúng ta tự viết -->
-    <link rel="stylesheet" href="css/css.css" type="text/css">
-</head>
-
-<body>
-    <div class="type-17">
-        <div class="container">
-
-            <div class="col-md-7" style="float: right;">
+        <div class="col-md-7" style="float: right;">
+            <div class="row">
                 <h3 class="product-title">Brown Women Casual HandBag</h3>
                 <div class="rating" style="display: flex;">
                     <div class="stars">
@@ -31,10 +14,12 @@
                     </div>
 
                     <span class="review" style="padding: 0px 5px 0 5px;">1 customer review</span>
-                    <span class="review" style=" border-left:1px solid gray ; padding-left:5px;">Add a review</span>
+                    <span class="review" style=" border-left:1px solid gray ; padding-left:5px;">Add a
+                        review</span>
 
                 </div>
-                <!-- <small class="text-muted">Giá cũ: <s><span>10,990,000.00 vnđ</span></s></small> -->
+            </div>
+            <div class="row">
                 <h4 class="price"> <span>$209.00 - $269.00</span></h4>
                 <p class="product-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio
                     magnam harum quibusdam quo, repudiandae corrupti est omnis molestias ex asperiores magni, quae ad
@@ -51,41 +36,46 @@
                     <button style="border:1px solid rgb(210, 208, 208) ;background: white;"> Extra Large</button>
                     <button style="border:1px solid rgb(210, 208, 208) ;background:white ;">Large</button>
                 </h5>
-
-                <div class="buttons_added qty">
-                    <button class="minus is-form" type="button" value="-">-</button>
-                    <input aria-label="quantity" class="input-qty" max="10" min="1" name="" type="number" value="1">
-                    <button class="plus is-form" type="button" value="+">+</button>
-                </div>
-                <div class="buttons_added">
-
-                    <button class="add" type="submit"
-                        style="font-weight: bold;font-size: 13px;"> <i
-                            class="fa fa-star"></i>ADD TO CART</button>
-                </div>
-
-
-                <div class="action">
-
-                    <a class="icon btn btn-default" href="#"><span class="fa fa-heart"></span></a>
-                    <a class="icon btn btn-default" href="#"><span class="fa fa-heart"></span></a>
-                    <a class="icon btn btn-default" href="#"><span class="fa fa-heart"></span></a>
-                    <a class="icon btn btn-default" href="#"><span class="fa fa-heart"></span></a>
-                    <a class="icon btn btn-default" href="#"><span class="fa fa-heart"></span></a>
-                    <a class="icon btn btn-default" href="#"><span class="fa fa-heart">Add to wishlist</span></a>
-
-                </div>
             </div>
 
 
 
+            <div class="row border">
+                <div class="buttons_added qty">
+                    <button class="minus is-form" type="button" value="-">-</button>
+                    <input aria-label="quantity" class="input-qty" max="10" min="1" name="" type="number"
+                        value="1">
+                    <button class="plus is-form" type="button" value="+">+</button>
+                </div>
+                <div class="buttons_added">
+
+                    <button class="add" type="submit" style="font-weight: bold;font-size: 13px">
+                        <i class="fa fa-star"></i>ADD TO CART</button>
+                </div>
+            </div>
 
 
+            <div class="row border">
+                <div class="action">
 
+                    <a><i class="fa-brands fa-facebook"></i></a>
+                    <a><i class="fa-brands fa-twitter"></i></a>
+                    <a><i class="fa-brands fa-linkedin-in"></i></a>
+                    <a><i class="fa-brands fa-google-plus-g"></i></a>
+                    <a><i class="fa-solid fa-envelope"></i></a>
+                    <a style="text-decoration: none"><i class="fa-solid fa-heart"></i><span>Add to
+                            wishlist</span></a>
+
+                </div>
+            </div>
 
         </div>
+
+
+
+
+
+
+
     </div>
-
-</body>
-
-</html>
+</div>
--- Porto-web/resources/views/clients/pages/detail_product.blade.php
+++ Porto-web/resources/views/clients/pages/detail_product.blade.php
@@ -1,4 +1,3 @@
-
 @extends('layouts.master')
 
 @section('title')
@@ -8,12 +7,14 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
         integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <link href="{{ asset('assets/clients/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="{{ asset('assets/clients/css/style.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
 @endsection
 
 @section('body')
-@include('clients.modules.module-19')
+    @include('clients.modules.module-17')
+    @include('clients.modules.module-19')
 @endsection
