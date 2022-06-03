506a573abf6a634613237f3d9d1acc37910bb0a2
voyen057 <voyen057@gmail.com>
2022-06-04 01:25:09 +0700

    edit module 17

--- Porto-web/resources/views/clients/modules/module-17.blade.php
+++ Porto-web/resources/views/clients/modules/module-17.blade.php
@@ -1,7 +1,6 @@
 <div class="module-17">
     <div class="container">
-
-        <div class="col-md-7" style="float: right;">
+        <div class="col-md-7 " style="float: right;">
             <div class="row">
                 <h3 class="product-title">Brown Women Casual HandBag</h3>
                 <div class="rating" style="display: flex;">
@@ -37,45 +36,31 @@
                     <button style="border:1px solid rgb(210, 208, 208) ;background:white ;">Large</button>
                 </h5>
             </div>
-
-
-
-            <div class="row border">
-                <div class="buttons_added qty">
-                    <button class="minus is-form" type="button" value="-">-</button>
-                    <input aria-label="quantity" class="input-qty" max="100" min="0" name="" type="number"
-                        value="1">
-                    <button class="plus is-form" type="button" value="+">+</button>
-                </div>
-                <div class="buttons_added">
-
-                    <button class="add" type="submit" style="font-weight: bold;font-size: 13px">
-                        <i class="fa fa-star"></i>ADD TO CART</button>
-                </div>
+            <div class="buttons_added qty">
+                <button class="minus is-form" type="button" value="-">-</button>
+                <input aria-label="quantity" class="input-qty" max="100" min="0" name="" type="number" value="1">
+                <button class="plus is-form" type="button" value="+">+</button>
+            </div>
+            <div class="buttons_added">
+                <button class="add" type="submit" style="font-weight: bold;font-size: 13px">
+                    <i class="fa fa-star"></i>ADD TO CART</button>
             </div>
 
 
-            <div class="row border">
-                <div class="action">
 
-                    <a><i class="fa-brands fa-facebook"></i></a>
-                    <a><i class="fa-brands fa-twitter"></i></a>
-                    <a><i class="fa-brands fa-linkedin-in"></i></a>
-                    <a><i class="fa-brands fa-google-plus-g"></i></a>
-                    <a><i class="fa-solid fa-envelope"></i></a>
-                    <a style="text-decoration: none"><i class="fa-solid fa-heart"></i><span>Add to
-                            wishlist</span></a>
 
-                </div>
+            <div class="action">
+
+                <a><i class="fa-brands fa-facebook"></i></a>
+                <a><i class="fa-brands fa-twitter"></i></a>
+                <a><i class="fa-brands fa-linkedin-in"></i></a>
+                <a><i class="fa-brands fa-google-plus-g"></i></a>
+                <a><i class="fa-solid fa-envelope"></i></a>
+                <a style="text-decoration: none"><i class="fa-solid fa-heart"></i><span>Add to
+                        wishlist</span></a>
+
             </div>
-
         </div>
-
-
-
-
-
-
 
     </div>
 </div>
--- Porto-web/resources/views/clients/modules/module-7.blade.php
+++ Porto-web/resources/views/clients/modules/module-7.blade.php
@@ -9,25 +9,25 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body> --}}
-    <div class="module-7 container">
-        <div class="banner-sale-home">
-            <div class="row">
-                <div class="col-md-9">
-                    <div class="big-sale">
-                        <h2>
-                            <strong>BIG SALE</strong>
-                            ALL NEW FASHION BRANDS ITEMS UP TO 70% OFF
-                            <small>Online Purchases Only</small>
-                        </h2>
-                    </div>
+<div class="module-7 container">
+    <div class="banner-sale-home">
+        <div class="row">
+            <div class="col-md-9">
+                <div class="big-sale">
+                    <h2>
+                        <strong>BIG SALE</strong>
+                        ALL NEW FASHION BRANDS ITEMS UP TO 70% OFF
+                        <small>Online Purchases Only</small>
+                    </h2>
                 </div>
-                <div class="col-md-3">
-                    <div class="view-sale text-center">
-                        <button class="btn-viewsale">VIEW SALE</button>
-                    </div>
+            </div>
+            <div class="col-md-3">
+                <div class="view-sale text-center">
+                    <button class="btn-viewsale">VIEW SALE</button>
                 </div>
             </div>
         </div>
     </div>
+</div>
 {{-- </body>
 </html> --}}
