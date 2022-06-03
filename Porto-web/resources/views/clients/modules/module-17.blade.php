fe0cd1ca65935f1c51b089fb700578e19f98c752
voyen057 <voyen057@gmail.com>
2022-06-04 02:02:51 +0700

    edit module 17

--- Porto-web/resources/views/clients/modules/module-17.blade.php
+++ Porto-web/resources/views/clients/modules/module-17.blade.php
@@ -19,7 +19,8 @@
                 </div>
             </div>
             <div class="row">
-                <h4 class="price"> <span>$209.00 - $269.00</span></h4>
+                <h4 class="price"> <span>$209.00 - $269.00</span>
+                </h4>
                 <p class="product-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio
                     magnam harum quibusdam quo, repudiandae corrupti est omnis molestias ex asperiores magni, quae ad
                     mollitia laudantium maxime dignissimos pariatur quam officiis, iste dolor a. Enim, repellat!</p>
@@ -36,26 +37,33 @@
                     <button style="border:1px solid rgb(210, 208, 208) ;background:white ;">Large</button>
                 </h5>
             </div>
-            <div class="buttons_added qty">
-                <button class="minus is-form" type="button" value="-">-</button>
-                <input aria-label="quantity" class="input-qty" max="100" min="0" name="" type="number" value="1">
-                <button class="plus is-form" type="button" value="+">+</button>
+            <div id="add">
+                <div class="buttons_added qty">
+                    <button class="minus is-form" type="button" value="-">-</button>
+                    <input aria-label="quantity" class="input-qty" max="100" min="0" name="" type="number"
+                        value="1">
+                    <button class="plus is-form" type="button" value="+">+</button>
+                </div>
+                <div class="buttons_added added">
+                    <button class="add" type="submit"
+                        style="font-weight: bold;font-size: 15px; height:3rem;width:12rem">
+                        <i class="fa fa-star"></i>ADD TO CART</button>
+                </div>
             </div>
-            <div class="buttons_added">
-                <button class="add" type="submit" style="font-weight: bold;font-size: 13px">
-                    <i class="fa fa-star"></i>ADD TO CART</button>
-            </div>
 
 
 
 
-            <div class="action">
 
-                <a><i class="fa-brands fa-facebook"></i></a>
-                <a><i class="fa-brands fa-twitter"></i></a>
-                <a><i class="fa-brands fa-linkedin-in"></i></a>
-                <a><i class="fa-brands fa-google-plus-g"></i></a>
-                <a><i class="fa-solid fa-envelope"></i></a>
+
+
+            <div class="action pt-3">
+
+                <a id="icon"><i class="fa-brands fa-facebook"></i></a>
+                <a id="icon"><i class="fa-brands fa-twitter"></i></a>
+                <a id="icon"><i class="fa-brands fa-linkedin-in"></i></a>
+                <a id="icon"><i class="fa-brands fa-google-plus-g"></i></a>
+                <a id="icon"><i class="fa-solid fa-envelope"></i></a>
                 <a style="text-decoration: none"><i class="fa-solid fa-heart"></i><span>Add to
                         wishlist</span></a>
 
