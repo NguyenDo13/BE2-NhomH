4da7acba94374a09f00eb3a2821a396126340ccb
NguyenDo13 <84115673+NguyenDo13@users.noreply.github.com>
2022-06-09 22:24:53 +0700

    fill data get from table product

--- Porto-web/app/Http/Controllers/CartController.php
+++ Porto-web/app/Http/Controllers/CartController.php
@@ -6,75 +6,39 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Cart;//sử dụng để truy vấn data bằng eloquent
 use App\Models\cart_details;
-
+use App\Models\Product;
 use Illuminate\Support\Facades\DB;//sử dụng khi truy vấn data bằng Query Builder (DB::)
 
 class CartController extends Controller
 {
     //dữ liệu dùng chung
     public $data = [];
-    private $idUser;
-    private $insert = ['2', '5', 'M', '4'];
+
     public function showCart(){
-        $idUser = rand(1,10);
+        //get id_user
+        $idUser = 1;
         $this->data['idUser'] = $idUser;
+
+        //get id_cart
         $idCart = Cart::where('id_user', $idUser)->value('id');
+
+        //kiem tra
         if($idCart == null){
             return 'user khong ton tai';
         }
-        $this->data['carts'] = cart_details::where('id_Cart', $idCart)->get();
-        return view('clients.pages.carts', $this->data);
-    }
-    //khởi tạo dữ liệu mẫu
-    public function initData()
-    {
-        //reset id
-        DB::update("ALTER TABLE carts AUTO_INCREMENT = 1;");
-        //insert cart
-        DB::table('carts')->insert([
-            'id_user' => 1,
-        ]);
-        //insert detail
-        DB::table('cart_details')->insert([
-            'id_cart' => 1,
-            'id_prod' => 1,
-            'size' => 'S',
-            'qty' => 4,
-        ]);
-        //get data
-        $carts = Cart::all();
-        $this->data['carts'] = $carts->fresh();
-        
-        $this->data['cart_details'] = DB::table('cart_details')->get();
-        return dd($this->data);
-    }
-    //lấy toàn bộ dữ liệu bảng
-    public function get()
-    {
-        $this->data['carts'] = Cart::all();
-        $this->data['cart_details'] = cart_detail::all();
 
-        return dd($this->data);
-    }
-    //xóa toàn bộ dữ liệu bảng
-    public function remove()
-    {
-        $this->data['carts'] = Cart::all();
-        foreach($this->data['carts'] as $item){
-            $this->data['carts'] = DB::table('carts')->delete($item['id']);
+        //get cart
+        $this->data['carts'] = cart_details::where('id_Cart', $idCart)->get()->toArray();
+        $this->data['products'] = [];
+        //get list prod
+        foreach ($this->data['carts'] as $item) {  
+            $Product = Product::where('id', $item['id_prod'])->get()->toArray();
+            array_push($this->data['products'], $Product);
         }
-        return dd($this->data['carts']);
-    }
-    // sử cột id_product có id là 1
-    public function update()
-    {
-        $this->data['carts'] = DB::table('carts')->where('id', 1)->update(['id_product' => 6]);
 
-        return dd($this->data['carts']);
-    }
+        //all data cart
 
-    public function getToMany(){
-        $this->data['carts'] = Cart::getDetail();
-        return dd($this->data['carts']);
+        // return dd($this->data['carts']);
+        return view('clients.pages.carts', $this->data);
     }
 }
--- Porto-web/routes/web.php
+++ Porto-web/routes/web.php
@@ -15,25 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/category', [ProductController::class, 'ShowAllProduct']);
-Route::get('/', function () {
-    return view('clients.pages.home');
-});
 
+Route::get('/cart', [CartController::class, 'showCart'])->name('cart');
 
-Route::get('/cart', [CartController::class, 'initData'])->name('cart');
-
-Route::get('/login', function(){
-    return view('clients.pages.login');
-});
-// Product detail
-Route::get('/product/{id}', [ProductController::class, 'getProductById']);
-// Product->category
-Route::get('/product', function () {
-    return view('clients.pages.detail_product');
-});
-// Route::get('/category', function () {
-//     return view('clients.pages.categories');
-// });
-Route::get('/', [ProductController::class, 'ShowFeatureProduct']);
 
