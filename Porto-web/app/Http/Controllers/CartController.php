00b70334a03053046d8adfe512eef417c093430d
NguyenDo13 <84115673+NguyenDo13@users.noreply.github.com>
2022-06-09 15:53:29 +0700

    make sortDeletes

--- Porto-web/app/Http/Controllers/CartController.php
+++ Porto-web/app/Http/Controllers/CartController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Cart;//sử dụng để truy vấn data bằng eloquent
 use App\Models\cart_detail;
+use Database\Seeders\cart_details;
+use Database\Seeders\carts;
 use Illuminate\Support\Facades\DB;//sử dụng khi truy vấn data bằng Query Builder (DB::)
 
 class CartController extends Controller
@@ -44,25 +46,41 @@ class CartController extends Controller
 
         return dd($this->data);
     }
+    //lấy toàn bộ dữ liệu bảng
+    public function add()
+    {
+        //reset id
+        DB::update("ALTER TABLE carts AUTO_INCREMENT = 1;");
+        //insert cart
+        DB::table('carts')->insert([
+            'id_user' => 11,
+        ]);
+        //insert detail
+        DB::table('cart_details')->insert([
+            'id_cart' => 1,
+            'id_prod' => 1,
+            'size' => 'S',
+            'qty' => 4,
+        ]);
+
+        return dd($this->data);
+    }
     //xóa toàn bộ dữ liệu bảng
     public function remove()
     {
-        $this->data['carts'] = Cart::all();
-        foreach($this->data['carts'] as $item){
-            $this->data['carts'] = DB::table('carts')->delete($item['id']);
-        }
+        $this->data['carts'] = cart_detail::where('id_cart', 1)->delete();
         return dd($this->data['carts']);
     }
     // sử cột id_product có id là 1
     public function update()
     {
-        $this->data['carts'] = DB::table('carts')->where('id', 1)->update(['id_product' => 6]);
-
+        // $this->data['carts'] = DB::table('carts')->where('id', 15)->update(['id_product' => 6]);
+        $this->data['carts'] = Cart::where('id',15)->update(['id_user' => 15]);
         return dd($this->data['carts']);
     }
 
-    public function getToMany(){
-        $this->data['carts'] = Cart::getDetail();
-        return dd($this->data['carts']);
-    }
+    // public function getToMany(){
+    //     $this->data['carts'] = Cart::getDetail();
+    //     return dd($this->data['carts']);
+    // }
 }
--- Porto-web/app/Models/Cart.php
+++ Porto-web/app/Models/Cart.php
@@ -6,18 +6,19 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 use App\Models\CartDetail;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 
 
 
 class Cart extends Model
 {
-    //khai báo bảng để sử dụng cho Eloquent, mặc định sẽ ngầm hiểu tên bảng là tên lớp viết thường + s
-    // protected $table = 'carts';
-    //khai báo khóa chính, mặc định sẽ ngầm hiểu khóa chính là thằng có increment
-    // protected $id = 'id';
-    //có thể khai báo thêm một vài trường để xử lý tiếp
-    public function getDetail(){
-        return $this->hasMany('CartDetail');
-    }
+    use SoftDeletes;
+
+    protected $table = 'carts';
+    protected $primaryKey = 'id';
+    
+    protected $fillable = [
+        'id', 'id_user',
+    ];
 }
