b8ec1f410eaea9fa34e8503b140001d46e5fce22
AnPhan <tlt2542001@gmail.com>
2022-06-11 10:13:24 +0700

    edit

--- Porto-web/app/Models/Product.php
+++ Porto-web/app/Models/Product.php
@@ -10,17 +10,4 @@ class Product extends Model
     use HasFactory;
     protected $table = 'products';
 
-
-    public function addProduct($data){
-        DB::insert('INSERT INTO `product`(`product_name`, `product_price`, `product_img`,
-         `product_description`, `product_feature`,
-         `stock`, `sale_amount`, `expire_date`, `manufacture_id`, `type_id`, `comment_id`,`created_at`) 
-         VALUES (?,?,?,?,?,?,?,?,?,?,?,?)',$data);
-    }
-    public function getDetail($id){
-        return DB::select('SELECT * FROM '.$this->table.' WHERE id = ?',[$id]);
-    }
-    public function deleteProduct($id){
-        return DB::delete("DELETE FROM $this->table WHERE id=?",[$id]);
-    }
 }
